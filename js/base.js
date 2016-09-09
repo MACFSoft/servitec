96eb44bdfa4ef15c1ea8d79a0d2315953e2d5688
MACFSoft <marco_anibal@hotmail.com>
2016-09-09 13:08:42 -0500

    Actulizaciion
    Reparado algunos errores de la forma de ejecutar las rutinas  sigue las
    fotos en formato JPG

--- js/base.js
+++ js/base.js
@@ -7,23 +7,28 @@ var rain=0;
 var localID;
 var ServerData;
 var at;
-var distan=25;
-var gpsopt = {maximumAge: 15000, timeout: 30000, enableHighAccuracy: true };
-
+var distan;
+var GPSOpt = {maximumAge: 3000, timeout: 5000, enableHighAccuracy: true };
+var CameraOpt = {EncodingType: 1, quality: 100, allowEdit: false }
 //=========================== V A R I A B L E S DATA ===========================
-var SO_Device="Android Virtual";       //Mintras la beta
-var Ver_Device="4.0.0";                //Mintras la beta
-var Uuid_Device="6A689277024D4DE4";    //Mintras la beta
-var Latit='3.9913054';                  //Mintras la beta
-var Long='-73.7651035';                 //Mintras la beta
-var Accur=1000;                        //Mintras la beta
+var SO_Device="Android Virtual";       //Mientras la beta
+var Ver_Device="4.0.0";                //Mientras la beta
+var Uuid_Device="6A689277024D4DE4";    //Mientras la beta
+var Latit='3.9913054';                 //Mientras la beta
+var Long='-73.7651035';                //Mientras la beta
+var Accur=1000;                        //Mientras la beta
+var Bruju=0;                           //Mientras la beta
+var Velocid=0;                            //Mientras la beta
 var fichero;
 
 // ============================ S e c c i o n e s ==============================
 document.addEventListener("deviceready", IniciarAplicacion, false);
 function ActGPS(){
-  localID = navigator.geolocation.watchPosition(GpsonSuccess,GpsonError,gpsopt);
+  localID = navigator.geolocation.watchPosition(GpsonSuccess,GpsonError,GPSOpt);
 } //ActGPS
+function ObtFoto(){
+  navigator.camera.getPicture(CamaraonSuccess, CamaraonError,CameraOpt);
+} //ObtFoto
 // ============================= F U N C I O N E S =============================
 function Barrain(){
  $("#loadin").attr('src','img/barra/barra'+barrain+'.png');
@@ -72,7 +77,6 @@ function CargarD(){
   $("#txt_user").val(localStorage.getItem('User'));
   $("#txt_pass").val(localStorage.getItem('Pass'));
 } //CargarD
-
 function getDistanceToCoords(lat1,lon1,lat2,lon2) {
   function _deg2rad(deg) {
     return deg * (Math.PI/180)
@@ -85,28 +89,37 @@ function getDistanceToCoords(lat1,lon1,lat2,lon2) {
     Math.cos(_deg2rad(lat1)) * Math.cos(_deg2rad(lat2)) *
     Math.sin(dLon/2) * Math.sin(dLon/2);
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-  var d = (R * c)/1000; // Distance in km/1000mts
-  return d.toFixed(3);
+  var d = (R * c); // Distance en Km
+    d = d*1000;    // En metros
+  //return d.toFixed(3);
+  return d.toFixed(0);
 } //Distancia en mts
 function GpsonError(e) {
   $('#maploc').attr('src', "img/mapa.png");
   alert("Por favor encender el GPS para continuar.");
 } //GpsonError
 function GpsonSuccess(position) {
-  //distan=getDistanceToCoords(Latit,Long,position.coords.latitude,position.coords.longitude);
-  //if (Accur > position.coords.accuracy || distan > 10) {
-    Latit = position.coords.latitude;
-    Long = position.coords.longitude;
-    Accur = position.coords.accuracy;
-    MapaPS("#maploc");
-  //}
+  distan=getDistanceToCoords(Latit,Long,position.coords.latitude,position.coords.longitude);
+  if (distan>10) {
+      Latit = position.coords.latitude;
+       Long = position.coords.longitude;
+    Velocid = position.coords.speed;
+      Bruju = position.coords.heading;
+      Accur = position.coords.accuracy;
+     MapaPS('#maploc');
+  }
+  if (Accur>position.coords.accuracy){
+      Latit = position.coords.latitude;
+       Long = position.coords.longitude;
+      Accur = position.coords.accuracy;
+    MapaPS('#maploc');
+  }
 } //GpsonSuccess
 function MapaPS(conten){
   Gpsurl = GMaps.staticMapURL({
   size: [300, 300],
-  lat: Latit,
-  lng: Long,
-  zoom:18,
+   lat: Latit,
+   lng: Long,
     marker: {
       lat: Latit,
       lng: Long,
@@ -114,19 +127,40 @@ function MapaPS(conten){
       color: 'green'
     }
   });
+//Cargando datos
 $(conten).attr('src', Gpsurl);
-$("#fidel").html("Error es: "+Accur+"mts");
-MError = "GpsD";
-MError = (Accur > 13 && Accur <  30)? "GpsM":MError;
-MError = (Accur > 10 && Accur <= 13)? "GpsR":MError;
-MError = (Accur >  7 && Accur <= 10)? "GpsA":MError;
-MError = (Accur >  5 && Accur <=  7 )? "GpsB":MError;
-MError = (Accur <= 5)? "GpsE":MError;
-MError += " btninfo";
+$('#fidel').html("Error es: "+Accur+"mts");
+$('#bruja').html(Bruju+"´");
+  MError = "GpsD";
+  MError = (Accur > 13 && Accur <  30)? "GpsM":MError;
+  MError = (Accur > 10 && Accur <= 13)? "GpsR":MError;
+  MError = (Accur >  7 && Accur <= 10)? "GpsA":MError;
+  MError = (Accur >  5 && Accur <=  7 )? "GpsB":MError;
+  MError = (Accur <= 5)? "GpsE":MError;
+ MError += " btninfo";
 $("#fidel").attr("class",MError);
-//console.log(MError);
-    //MapaPS('#maploc');
+// Enviar datos del usiario al servidor
+ //Evaluar los datos a enviar
 } //MapaPS
+function CamaraonError(message){
+  //alert('Se presento un error: ' + message);
+  window.location.assign("#page2");
+} //camaraonError
+function CamaraonSuccess(rutaImagen){
+  document.getElementById("imgCamara").src = rutaImagen;
+  fichero = rutaImagen;
+} //camaraonSuccess
+
+
+function uploadonError(error) {
+	//window.location.assign("#page4");
+    alert('Se presento un error: ' + error.source);
+    //$("#razon2").fadeIn(1000);
+} //uploadonError
+function uploadonSuccess(r) {
+  alert("subido "+r.source);
+    window.location.assign("#page0");
+} //uploadonSuccess
 
 
 
@@ -198,7 +232,33 @@ function TraerList(){
     }
   });
 } //TraerList
-
+function EnviaDATA(){
+var optitons = new FileUploadOptions();
+  optitons.fileKey="file";
+  optitons.fileName=fichero.substr(fichero.lastIndexOf('/')+1);
+  //optitons.mimeType="image/jpeg";  //optitons.mimeType="image/png";
+  optitons.mimeType="image/png";  //optitons.mimeType="image/jpeg";
+  optitons.chunkedMode=true;
+var params = {};
+  //Datos origen
+    params.uuid_d = Uuid_Device;
+    params.latitud = Latit;
+    params.longitud = Long;
+    params.error = Accur;
+    //params.bruja = Bruju;
+    //params.velo = Velocid;
+  //Datos clasificacion
+    params.tablero  = $("#sl-tablero").val();
+    params.pedestal = $("#sl-pedestal").val();
+    params.anclaje  = $("#sl-anclaje").val();
+    params.vision   = $("#sl-vision").val();
+    params.accion   = $("#sl-accion").val();
+    params.obser    = $("#txt-obser").val();
+  optitons.params = params;
+  var ft = new FileTransfer();
+  //alert(params);
+  ft.upload(fichero, ServerData+"/updata.php", uploadonSuccess, uploadonError, optitons);
+} //EnviaDATA
 
 
 
@@ -251,4 +311,46 @@ $(document).on('pageshow',"#page1",function(){
 $(document).on('pageshow',"#page2",function(){
   window.clearInterval(at);
   ActGPS();
+  //MapaPS('#maploc');
+  //console.log( "hola page on");
 }); //page2
+$(document).on('pageshow',"#page3",function(){
+  navigator.geolocation.clearWatch(localID);
+}); //page3
+ //??&nbsp;Pahe 4 EnviaDATA
+
+$(document).on('pageshow',"#page5",function(){
+  if (localStorage.getItem('Setup')==1){
+  //Calificacion pedestal
+    ArrySelect = localStorage.getItem('Tablero').split('|');
+    $.each(ArrySelect, function(idx,val) {
+      idx++;
+      $('#sl-tablero').append('<option value="'+idx+'">'+val+'</option>');
+    });
+  //Calificacion pedestal
+    ArrySelect = localStorage.getItem('Pedestal').split('|');
+    $.each(ArrySelect, function(idx,val) {
+      idx++;
+      $('#sl-pedestal').append('<option value="'+idx+'">'+val+'</option>');
+    });
+  //Calificacion anclaje */
+    ArrySelect = localStorage.getItem('Anclaje').split('|');
+    $.each(ArrySelect, function(idx,val) {
+      idx++;
+      $('#sl-anclaje').append('<option value="'+idx+'">'+val+'</option>');
+    });
+  //Calificacion vista
+    ArrySelect = localStorage.getItem('Vista').split('|');
+    $.each(ArrySelect, function(idx,val) {
+      idx++;
+      $('#sl-vision').append('<option value="'+idx+'">'+val+'</option>');
+    });
+  //Clasificacion de  la accion
+    ArrySelect = localStorage.getItem('Accion').split('|');
+    $.each(ArrySelect, function(idx,val) {
+      idx++;
+      $('#sl-accion').append('<option value="'+idx+'">'+val+'</option>');
+    });
+  } //if Setup
+}); //page5
+
